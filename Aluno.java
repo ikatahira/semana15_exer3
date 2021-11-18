095f87475610a831d47fcd764b420e4ce91509cf
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-11-18 01:22:42 +0000

    exer3

---
+++ Aluno.java
@@ -0,0 +1,15 @@
+public class Aluno{
+
+private String nome;
+private String matricula;
+private String curso;
+private String java;
+private String matematica;
+private String iot;
+private double  P1;
+private double P2;
+
+
+
+
+}
