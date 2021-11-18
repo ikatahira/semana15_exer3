4cf82a43be93b543cacc9d2e3bc9e906c439da5c
ikatahira <isaque.katahira@fatec.sp.gov.br>
2021-11-18 01:39:19 +0000

    resolução 3

--- Aluno.java
+++ Aluno.java
@@ -15,7 +15,55 @@ public void setNome(String nome){
 public String getNome(){
     return nome;
 }
+public String getMatricula() {
+    return matricula;
+}
+public void setMatricula(String matricula) {
+    this.matricula = matricula;
+}
+public String getCurso() {
+    return curso;
+}
+public void setCurso(String curso) {
+    this.curso = curso;
+}
+public String getJava() {
+    return java;
+}
+public void setJava(String java) {
+    this.java = java;
+}
+public String getMatematica() {
+    return matematica;
+}
+public void setMatematica(String matematica) {
+    this.matematica = matematica;
+}
+public String getIot() {
+    return iot;
+}
+public void setIot(String iot) {
+    this.iot = iot;
+}
+public double getP1() {
+    return P1;
+}
+public void setP1(double p1) {
+    P1 = p1;
+}
+public double getP2() {
+    return P2;
+}
+public void setP2(double p2) {
+    P2 = p2;
+}
 
-
+public boolean aprovado(){
+    double notafinal=(P1+P2)/(2.0);
+    if(notafinal>=7)
+        return true;
+    else
+        return false;
+}
 
 }
--- Teste.java
+++ Teste.java
@@ -4,7 +4,21 @@ public class Teste{
     public static void main(String[] args) {
         Aluno aluno=new Aluno();
 
-        
+        aluno.setCurso("Big Data no Agronegócio");
+        aluno.setJava("Java I Orientação à Objetos");
+        aluno.setP1(9);
+        aluno.setP2(8);
+        if(aluno.aprovado()){
+            System.out.println("Passou");
+            System.out.println("Nota da P1: "+ aluno.getP1());
+            System.out.println("Nota da P2: "+ aluno.getP2());
+        }
+        else{
+            System.out.println("Tente novamente");
+            System.out.println("Nota da P1: "+ aluno.getP1());
+            System.out.println("Nota da P2: "+ aluno.getP2());
+        }
+
 
     }
 }
